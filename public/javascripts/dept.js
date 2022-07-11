fc9c8859fd7f4865381349df75f7aac9d8392889
oceanneesparcieux <oceanne.esparcieux@cabinetoffice.gov.uk>
2022-07-11 08:13:46 +0100

    2nd commit including results page

--- public/javascripts/dept.js
+++ public/javascripts/dept.js
@@ -1,41 +1,38 @@
 var dept = [
-  "Attorney General's Office"',
-  'Crown Prosecution Service',
-  'Government Legal Department',
-  "Serious Fraud Office",
-  "HM Crown Prosecution Service",
-  "Inspectorate",
-  "Cabinet Office",
-  "Crown Commercial Service",
-  "Government Property Agency",
-  "Civil Service Commission",
-  "Equality and Human Rights Commission",
-  "Advisory Committee on Business Appointments",
-  "Boundary Commission for England",
-  "Boundary Commission for Wales",
-  "Committee on Standards in Public Life",
-  "House of Lords Appointments Commission",
-  "Security Vetting Appeals Panel",
-  "Senior Salaries Review Body",
-  "Commissioner for Public Appointments",
-  "Government Equalities Office",
-  "Government Estates Management",
-  "HM Land Registry",
-  "Ofgem",
-  "Companies House",
-  "The Insolvency Service",
-  "Intellectual Property Office",
-  "Met Office",
-  "UK Space Agency",
-  "Department for Digital, Culture, Media & Sport",
-  "The National Archives",
-  "Arts Council England",
-  "Environment Agency",
-  "Department for International Trade",
-  "Driver and Vehicle Licensing Agency",
-  "HM Land Registry",
-  "HM Revenue & Customs",
+  "Attorney General's Office"
+  'Crown Prosecution Service'
+  'Government Legal Department'
+  "Serious Fraud Office"
+  "HM Crown Prosecution Service"
+  "Inspectorate"
+  "Cabinet Office"
+  "Crown Commercial Service"
+  "Government Property Agency"
+  "Civil Service Commission"
+  "Equality and Human Rights Commission"
+  "Advisory Committee on Business Appointments"
+  "Boundary Commission for England"
+  "Boundary Commission for Wales"
+  "Committee on Standards in Public Life"
+  "House of Lords Appointments Commission"
+  "Security Vetting Appeals Panel"
+  "Senior Salaries Review Body"
+  "Commissioner for Public Appointments"
+  "Government Equalities Office"
+  "Government Estates Management"
+  "HM Land Registry"
+  "Ofgem"
+  "Companies House"
+  "The Insolvency Service"
+  "Intellectual Property Office"
+  "Met Office"
+  "UK Space Agency"
+  "Department for Digital, Culture, Media & Sport"
+  "The National Archives"
+  "Arts Council England"
+  "Environment Agency"
+  "Department for International Trade"
+  "Driver and Vehicle Licensing Agency"
+  "HM Land Registry"
+  "HM Revenue & Customs"
   ]
-var element = document.querySelector('#dept')
-var id = 'autocomplete-default'
-accessibleAutocomplete({element: element, id: id, source: dept})
