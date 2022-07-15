f5b62b1332b4d43cbd4a0650d79dcaad5e9952bc
oceanneesparcieux <oceanne.esparcieux@cabinetoffice.gov.uk>
2022-07-15 16:37:18 +0100

    job library commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,20 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+// Start by hiding the conditional area. So put a div around the whole enter trust information with an ID of conditional or whatever you want to call it. We will call this variable a from now on.
+//var a = $("#conditional");
+//Then hide the variable a by default
+//a.hide();
+//Listen for a change to the checkbox, in this case the name of the checkbox is waste. So whenever you tick one of those boxes the following code will fire each time.
+//$("input[name='job-button']").change(function() {
+    //Create a new variable called A_is_checked with a value which checks to see if the last item of the checboxes is checked
+//var A_is_checked = $("input[name='job-button']").is(':checked');
+    //If the user has checked the last item (variable A_is_checked) then
+//if (A_is_checked) {
+      //Show A (or the conditional)
+ //a.show();
+//} else {
+      //Else we will assume that it hasn't been ticked so don't show the conditional
+//  a.hide();
+  //  }
+//});
