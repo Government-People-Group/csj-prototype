89b053737e3a5e37d8fd789bff2e0c9170286bcd
oceanneesparcieux <oceanne.esparcieux@cabinetoffice.gov.uk>
2022-07-25 09:08:25 +0100

    interview commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -57,3 +57,19 @@ a.show();
   a.hide();
  }
 });
+var a = $("#conditional-panel-members");
+//Then hide the variable a by default
+a.hide();
+//Listen for a change to the checkbox, in this case the name of the checkbox is waste. So whenever you tick one of those boxes the following code will fire each time.
+$("input[name='panel-selected']").change(function() {
+    //Create a new variable called A_is_checked with a value which checks to see if the last item of the checboxes is checked
+var A_is_checked = $("input[name='panel-selected']:first").is(':checked');
+    //If the user has checked the last item (variable A_is_checked) then
+if (A_is_checked) {
+      //Show A (or the conditional)
+a.show();
+} else {
+      //Else we will assume that it hasn't been ticked so don't show the conditional
+  a.hide();
+ }
+});
