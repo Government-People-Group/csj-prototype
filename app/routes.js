845430f28e5f6eba56218667de0b8ba079711516
oceanneesparcieux <oceanne.esparcieux@cabinetoffice.gov.uk>
2022-08-16 15:16:39 +0100

    assessment method 3 passing data commit

--- app/routes.js
+++ app/routes.js
@@ -102,4 +102,16 @@ router.get('/campaign-set-up/job-library-profession-2', function (req, res) {
             break
           }
           })
+
+// assessment methods V3
+
+router.get('/campaign-set-up/assessment-method-1/assess', function (req, res) {
+
+    res.render('campaign-set-up/assessment-method-1/assess', {
+
+    })
+  })
+  router.post('/campaign-set-up/assessment-method-1/assess', function (req, res) {
+      res.redirect('cv')
+})
 module.exports = router
